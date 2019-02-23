41bb9401207d529ebd826c8472c99fc01ca78bf1
Usher, Tracy L <usher@slac.stanford.edu>
2019-02-23 08:47:37 -0800

    Renamed version, more generic with this name (ie both raw and wire data can go through this)

---
+++ lareventdisplay/EventDisplay/wfHitDrawers/IWaveformDrawer.h
@@ -0,0 +1,36 @@
+///////////////////////////////////////////////////////////////////////
+///
+/// \file   IWaveformDrawer.h
+///
+/// \brief  This provides an interface for tools which are tasked with
+///         drawing the "wire" data (deconvolved waveforms)
+///
+/// \author T. Usher
+///
+////////////////////////////////////////////////////////////////////////
+
+#ifndef IWaveformDrawer_H
+#define IWaveformDrawer_H
+
+#include "fhiclcpp/ParameterSet.h"
+#include "larcoreobj/SimpleTypesAndConstants/RawTypes.h" // raw::ChannelID_t
+#include "nutools/EventDisplayBase/View2D.h"
+#include "TF1.h"
+
+namespace evdb_tool
+{
+    class IWaveformDrawer
+    {
+    public:
+        virtual ~IWaveformDrawer() noexcept = default;
+        
+        virtual void configure(const fhicl::ParameterSet& pset)           = 0;
+        virtual void Fill(evdb::View2D&, raw::ChannelID_t&, float, float) = 0;
+        virtual void Draw(const std::string&)                             = 0;
+        
+        virtual float getMaximum() const                                  = 0;
+        virtual float getMinimum() const                                  = 0;
+    };
+}
+
+#endif
